76b01b7c77e57049dc53c08e9b75e286c80987a2
Pc <alxzwl@gmail.com>
2022-05-21 22:39:24 -0500

    :art: Consulta de eliminacion en Hibernate

--- src/main/java/conexionHibernate/ActualizaClientes.java
+++ src/main/java/conexionHibernate/ActualizaClientes.java
@@ -13,14 +13,31 @@ public class ActualizaClientes {
 		Session miSession = miFactory.openSession();
 
 		try {
-			int clienteId = 2;
-			miSession.beginTransaction();
+			// Consulta de actualizacion por Id
+			/*
+			 * int clienteId = 2; miSession.beginTransaction();
+			 * 
+			 * Clientes miCliente = miSession.get(Clientes.class, clienteId);
+			 * miCliente.setNombre("Tommi");
+			 * 
+			 * miSession.getTransaction().commit();
+			 * System.out.println("Registro actualizado de forma exitosa en la BBDD");
+			 */
 
-			Clientes miCliente = miSession.get(Clientes.class, clienteId);
-			miCliente.setNombre("Tommi");
+			// Consulta de actualizacion con el operador like
+			/*
+			 * miSession.beginTransaction(); miSession.
+			 * createQuery("update Clientes set apellidos='Ramirez' where apellidos like 'D%'"
+			 * ).executeUpdate();
+			 */
+
+			// Consulta de eliminacion con el operador like
+			miSession.beginTransaction();
+			miSession.createQuery("delete Clientes where direccion='Golla'").executeUpdate();
 
 			miSession.getTransaction().commit();
-			System.out.println("Registro actualizado de forma exitosa en la BBDD");
+			System.out.println("Registro eliminado de forma exitosa en la BBDD");
+
 			miSession.close();
 		} finally {
 			miFactory.close();
